aeae69f9688fea158d658707deecf3d1758830f0
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2018-12-04 21:24:50 +0100

    Switch off wifi to save power

--- ESP8266_AVR_Servo_MoodyUselessMachine_FingerDoor_EEPROM/ESP8266_AVR_Servo_MoodyUselessMachine_FingerDoor_EEPROM.ino
+++ ESP8266_AVR_Servo_MoodyUselessMachine_FingerDoor_EEPROM/ESP8266_AVR_Servo_MoodyUselessMachine_FingerDoor_EEPROM.ino
@@ -18,12 +18,14 @@
 
 #ifdef HWMODE_ESP01 // ESP-01
 // TODO: test
+#include <ESP8266WiFi.h>
 const int switchPin      = 3;  // GPIO3, RX port
 const int fingerServoPin = 0;  // GPIO0
 const int doorServoPin   = 2;  // GPIO2
 
 #else
 #ifdef ESP8266 // NodeMCU
+#include <ESP8266WiFi.h>
 const int switchPin      = D5; // GPIO14
 const int fingerServoPin = D6; // GPIO12
 const int doorServoPin   = D7; // GPIO13
@@ -481,6 +483,10 @@ void setup() {
   DEBUG_SERIAL.flush();
 #endif
 
+#ifdef ESP8266
+  WiFi.mode(WIFI_OFF);
+#endif
+  
   eeprom_init();
 
   pinModeGpio(switchPin);
