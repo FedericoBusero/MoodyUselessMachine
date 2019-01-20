1c83209c2ba15ecffce3c95bfdf6cd30744d2635
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2019-01-20 15:00:01 +0100

    fix crash in sequence10 (music sequence)

--- ESP8266_AVR_ServoWs2812Buzzer_MoodyUselessMachine/ESP8266_AVR_ServoWs2812Buzzer_MoodyUselessMachine.ino
+++ ESP8266_AVR_ServoWs2812Buzzer_MoodyUselessMachine/ESP8266_AVR_ServoWs2812Buzzer_MoodyUselessMachine.ino
@@ -722,16 +722,17 @@ void sequence9()
 
 void sequence10()
 {
+  char songname[20+1];
+  
   currentslowdown = 200;
 #ifdef BUZZER_PIN
-  int songnr = random(NUMSONGS) + 1;
+  int songnr = random(NUMSONGS);
   player.setSong(songlist[songnr]); // or take one song e.g. song_P1
+  player.getName(songname,20);
 #ifdef DEBUG_SERIAL
-  char songname[20+1];
   DEBUG_SERIAL.print("Start song nr ");
   DEBUG_SERIAL.println(songnr);
   DEBUG_SERIAL.print("Name: ");
-  player.getName(songname,20);
   DEBUG_SERIAL.println(songname);
 #endif
 #endif
